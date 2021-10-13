a888ebf2f9084933ff367a39fd03f4ba3c8961c4
bennoghana <bennoghana@gmail.com>
2021-10-13 00:30:13 +0000

    change in create todo view

--- lib/views/create_todo_view.dart
+++ lib/views/create_todo_view.dart
@@ -1,40 +1,174 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:todo_app/utilities/utils.dart';
 
-class CreateTodoView extends StatelessWidget {
-  CreateTodoView({Key? key}) : super(key: key);
+class CreateTodoView extends StatefulWidget {
+  const CreateTodoView({Key? key}) : super(key: key);
 
+  @override
+  State<CreateTodoView> createState() => _CreateTodoViewState();
+}
+
+class _CreateTodoViewState extends State<CreateTodoView> {
   final TextEditingController _titleController = TextEditingController();
+
+  final TextEditingController _descriptionController = TextEditingController();
+
   final TextEditingController _dateController = TextEditingController();
-  final TextEditingController _descController = TextEditingController();
-  final TextEditingController timeController = TextEditingController();
+
+  final TextEditingController _timeController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Create  New Todo'),
-        ),
-        body: Form(
-          child: ListView(children: [
+      appBar: AppBar(
+        title: const Text('Create new Todo'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: [
             TextFormField(
-                keyboardType: TextInputType.text,
-                textCapitalization: TextCapitalization.sentences,
-                controller: _titleController,
-                decoration: InputDecoration(
-                    labelText: 'Title',
-                    hintText: "Enter your title here",
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
-                    labelStyle: Theme.of(context).textTheme.bodyText1)),
-            SizedBox(height: 15),
-            TextFormField(
-              keyboardType: TextInputType.multiline,
+              controller: _titleController,
+              keyboardType: TextInputType.text,
               textCapitalization: TextCapitalization.sentences,
-              controller: _descController,
-              decoration: const InputDecoration(
-                labelText: "Description",
-                hintText: "Please  describe your task here",
+              decoration: InputDecoration(
+                  labelText: "Title",
+                  labelStyle: Theme.of(context).textTheme.bodyText1,
+                  hintText: 'Please enter a title',
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  focusedBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: customBlue))),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter a title';
+                }
+              },
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            TextFormField(
+              controller: _descriptionController,
+              keyboardType: TextInputType.multiline,
+              maxLines: 5,
+              textCapitalization: TextCapitalization.sentences,
+              decoration: InputDecoration(
+                  labelText: 'Description',
+                  labelStyle: Theme.of(context).textTheme.bodyText1,
+                  hintText: 'Please enter a description',
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  focusedBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: customBlue))),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter a description';
+                }
+              },
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    onTap: () {
+                      showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime.now(),
+                        lastDate: DateTime.now().add(const Duration(days: 365)),
+                      ).then((value) {
+                        setState(() {
+                          print(value);
+                          _dateController.text =
+                              DateFormat.yMMMMd().format(value!);
+                        });
+                      });
+                    },
+                    controller: _dateController,
+                    keyboardType: TextInputType.datetime,
+                    maxLines: 1,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration: InputDecoration(
+                        labelText: 'Date',
+                        labelStyle: Theme.of(context).textTheme.bodyText1,
+                        hintText: 'Please enter a date',
+                        floatingLabelBehavior: FloatingLabelBehavior.never,
+                        focusedBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(color: customBlue))),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Select enter a date';
+                      }
+
+                      if (value == DateFormat.yMMMMd().format(DateTime.now())) {
+                        return 'You selected today\'s date';
+                      }
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: TextFormField(
+                    onTap: () {
+                      showTimePicker(
+                              context: context, initialTime: TimeOfDay.now())
+                          .then((value) {
+                        setState(() {
+                          _timeController.text = value!.format(context);
+                        });
+                      });
+                    },
+                    controller: _timeController,
+                    keyboardType: TextInputType.datetime,
+                    maxLines: 1,
+                    textCapitalization: TextCapitalization.sentences,
+                    decoration: InputDecoration(
+                        labelText: 'Time',
+                        labelStyle: Theme.of(context).textTheme.bodyText1,
+                        hintText: 'Please enter a time',
+                        floatingLabelBehavior: FloatingLabelBehavior.never,
+                        focusedBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(color: customBlue))),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Select enter a time';
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 35),
+            TextButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  print(_titleController.text);
+                  print(_descriptionController.text);
+                  print(_dateController.text);
+                  print(_timeController.text);
+                } else {
+                  print('please enter data');
+                }
+              },
+              child: const Text(
+                'create Todo',
+                style: TextStyle(color: Colors.white),
               ),
+              style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(16),
+                  backgroundColor: customBlue),
             )
-          ]),
-        ));
+          ],
+        ),
+      ),
+    );
   }
 }
